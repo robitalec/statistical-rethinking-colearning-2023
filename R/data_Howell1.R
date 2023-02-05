2db8ac8f01ca7eded8ecd4587863e9894d156d1b
Alec L. Robitaille <robit.alec@gmail.com>
2023-02-05 12:54:31 -0330

    add check if rethinking is loaded

--- R/data_Howell1.R
+++ R/data_Howell1.R
@@ -1,7 +1,11 @@
-data_Howell1 <- function() {
-  data(Howell1)
+data_Howell <- function() {
+	if (!'rethinking' %in% .packages()) {
+		stop('please load the rethinking package')
+	}
+
+	data(Howell1)
   DT <- data.table(Howell1)
   DT[, sex := .GRP, by = male]
-  
+
   return(DT[, .(height, weight, age, sex)])
 }
