7d95b967f2b62510af3b2e30de137210f7da8b6e
Alec L. Robitaille <robit.alec@gmail.com>
2023-02-10 16:10:09 -0400

    fst _targets.R

---
+++ _targets.R
@@ -0,0 +1,60 @@
+# === Targets -------------------------------------------------------------
+# Alec L. Robitaille
+
+
+
+# Source ------------------------------------------------------------------
+lapply(dir('R', '*.R', full.names = TRUE), source)
+
+
+
+# Options -----------------------------------------------------------------
+# Targets
+tar_option_set(format = 'qs')
+
+# Stan
+options(mc.cores = 2,
+				scipen = 999,
+				digits = 2)
+
+
+# Targets: homework 2 -----------------------------------------------------
+targets_h02 <- c(
+	tar_target(
+		Howell_lt_12,
+		data_Howell()[age < 12]
+	),
+	tar_target(
+		m_h02_q02,
+		brm(
+			weight ~ 1 + age,
+			prior = c(
+				prior(normal(4, 0.5), Intercept),
+				prior(normal(4, 1), b),
+				prior(exponential(1), sigma)
+			),
+			data = Howell_lt_12
+		)
+	),
+	tar_target(
+		m_h02_q03,
+		brm(
+			weight ~ age + sex,
+			prior = c(
+				prior(normal(4, 0.5), Intercept),
+				prior(normal(4, 1), b),
+				prior(exponential(1), sigma)
+			),
+			data = Howell_lt_12
+		)
+	)
+)
+
+
+
+
+
+
+# Targets: all ------------------------------------------------------------
+# Automatically grab all the "targets_*" lists above
+lapply(grep('targets', ls(), value = TRUE), get)
