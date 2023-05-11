3b3675c958c73f107beccba116871ca6cfc3085a
Alec L. Robitaille <robit.alec@gmail.com>
2023-05-11 09:16:11 -0300

    add lme4, brms, rethinking models

--- scripts/interactions.R
+++ scripts/interactions.R
@@ -8,6 +8,8 @@ library(ggplot2)
 library(data.table)
 library(palmerpenguins)
 library(lme4)
+library(brms)
+library(rethinking)
 
 
 # Data --------------------------------------------------------------------
@@ -26,6 +28,22 @@ ggplot(penguins, aes(body_mass_g, bill_length_mm, color = sex)) +
 
 
 
+
 # Model -------------------------------------------------------------------
-m <- glm(bill_length_mm ~ body_mass_g * sex, data = penguins)
+complete_penguins <- penguins[complete.cases(penguins)]
+
+m_glm <- glm(bill_length_mm ~ body_mass_g * sex, data = complete_penguins)
+
+m_brm <- brm(bill_length_mm ~ body_mass_g * sex, data = complete_penguins)
+
+m_rethink <- ulam(
+	alist(
+		bill_length_mm ~ dnorm(mu, sigma),
+		mu <- alpha + beta[sex] * body_mass_g,
+		alpha ~ dnorm(0, 10),
+		beta[sex] ~ dnorm(0, 1),
+		sigma ~ dexp(1)
+	),
+	data = complete_penguins
+)
 
