3c9ebef88ff092abf7cf4d9ef873a09799506aa1
Alec L. Robitaille <robit.alec@gmail.com>
2024-01-18 14:08:31 -0400

    more pkgs

--- R/packages.R
+++ R/packages.R
@@ -1,13 +1,16 @@
 # Packages ----------------------------------------------------------------
 # Alec L. Robitaille
 
+
 library(targets)
 library(tarchetypes)
+library(qs)
 
 # library(conflicted)
 
 library(data.table)
 
+library(devtools)
 
 library(ggdag)
 library(dagitty)
@@ -18,9 +21,9 @@ library(patchwork)
 library(ggplot2)
 theme_set(theme_bw())
 
-
 library(knitr)
 
+library(cmdstanr)
 library(rethinking)
 library(brms)
 library(tidybayes)
@@ -29,3 +32,10 @@ library(emmeans)
 library(mice)
 
 library(janitor)
+
+
+# Installs ----------------------------------------------------------------
+# Sys.setenv(DOWNLOAD_STATIC_LIBV8 = 1)
+# install.packages("V8")
+# install.packages("cmdstanr", repos = c("https://mc-stan.org/r-packages/", getOption("repos")))
+# devtools::install_github("rmcelreath/rethinking")
